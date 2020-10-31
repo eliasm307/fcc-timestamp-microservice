28f9553c153acdcd130e8b6b340472c79540bfe1
eliasm307 <eliaschakomangoro@gmail.com>
2020-10-31 13:37:47 +0000

    With FCC tests passing

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+// log all requests
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
@@ -22,11 +28,44 @@ app.get("/", function (req, res) {
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+}); 
+
+// API timestamp endpoint 
+app.get(["/api/timestamp/", "/api/timestamp/:timestamp/"], function (req, res) {
+
+  console.log("GET Request received with inputTimestamp:", req.params.timestamp);
+
+  const timestamp = isNaN(req.params.timestamp) ? req.params.timestamp : parseFloat(req.params.timestamp)
+
+  let date = timestamp ? new Date(timestamp) : new Date();
+
+  console.log({
+    inputTimestamp: req.params.timestamp, 
+    castedTimestamp: timestamp,
+    typeofCastedTimestamp: typeof timestamp,
+    parsedDate: date,
+    typeOfDate: typeof date,
+  });
+
+  if(isNaN(date)) {
+    console.log("Invalid timestamp/date received")
+    res.json({
+      error : "Invalid Date"
+    });
+
+  }
+  else {
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+  }
+
 });
 
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
